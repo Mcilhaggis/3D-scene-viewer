2ea1021102ce512d59f4c9a9f54b6694fb5f6e18
Rachael McIlhagga <rmcilhagga@tvo.org>
2022-10-03 20:55:00 -0400

    adding camera, cibe, scene components, systems and vendor files

---
+++ src/SceneViewer/SceneViewer.js
@@ -0,0 +1,31 @@
+import { createCamera } from './components/camera.js';
+import { createCube } from './components/cube.js';
+import { createScene } from './components/scene.js';
+
+import { createRenderer } from './systems/renderer.js';
+import { Resizer } from './systems/Resizer.js';
+
+// These variables are module-scoped: we cannot access them
+// from outside the module
+let camera;
+let renderer;
+let scene;
+
+class SceneViewer {
+    // 1. Create an instance of the World app
+    constructor(container) {
+        camera = createCamera();
+        scene = createScene();
+        renderer = createRenderer();
+        container.append(renderer.domElement);
+
+    }
+
+    // 2. Render the scene
+    render() {
+        renderer.render(scene, camera);
+
+    }
+}
+
+export { SceneViewer };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import './style.css';
+import {SceneViewer} from './SceneViewer/SceneViewer'
+import './style.css'
+
+// create the main function
+function main() {
+    const container = document.querySelector('#scene-container');
+
+    const viewer = new SceneViewer(container);
+    viewer.render();
+
+}
+  
+  // call main to start the app
+  main();
