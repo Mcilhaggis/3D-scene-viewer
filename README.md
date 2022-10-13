f2594fbcdc40899a0f0f825e938bfae3fdcac507
Rachael McIlhagga <rmcilhagga@tvo.org>
2022-10-13 14:08:36 -0400

    parse json to send file name to scene render

--- README.md
+++ README.md
@@ -9,3 +9,26 @@ For continous monitoring for changes:
 ```
 webpack --watch 
 ```
+
+
+##Wants: 
+• Import data from json files
+• keyboard navigation 
+• 
+• 
+
+
+##Stretch goals
+• Clickable controls to move and rotate object
+• 
+• 
+• 
+
+
+
+##Concerns to tackle from Sketchfab integration:
+• No movement of object with keyboard when in orbit mode, and limited movement in first-person mode
+• Tab focus only visible on top left two elements
+• Tab interactivity is spotty, and only works for some buttons. It’s hard to tell which without a visual indication, and the functionality seems to be spotty, as I’m not getting the same results as just clicking on the buttons
+• No screen reader description (known)
+•The bottom of the I-Frame is cut off when viewed in fullscreen at 200%
--- src/index.js
+++ src/index.js
@@ -1,27 +1,43 @@
 import { SceneViewer } from './SceneViewer/SceneViewer'
 import './style.css'
-
 // create the main function but wait for the models to initalise 
+
+async function buildScene() {
+  let viewer;
+  let sceneContainers = document.querySelectorAll('.testingFind')
+
+  // get the json for each scene
+  for (let item of sceneContainers) {
+    const _configFile = item.dataset.config;
+    let dataGrab = await fetch(_configFile)
+    let json = await dataGrab.json()
+    let _config = json
+    viewer = new SceneViewer(item, _config);
+  }
+    return viewer;
+}
+
 async function main() {
-    const container = document.querySelector('#scene-container');
+  try {
+    let result = await buildScene()
+    await result.init();
+    result.start();
 
+  } catch (error) {
+    // do something with the error, so that you can see it
+    console.log(error)
+    // res.status(400).send('An unexpected error occurred')
+  }
 
-    
-    const viewer = new SceneViewer(container);
-    // complete async tasks
-    await viewer.init();
-    // Switch to a render load for improvement of running on lower power devices 
-    // produce a single frame (render on demand)
-    // viewer.render();
-
-    // rendering on demand, we are now only generating new frames when the user interacts with the scene and moves the camera. As soon as you move the controls, sure enough, a new frame will be created and the texture will show up.
-
-    // As a result, you also need to generate a new frame after the texture has loaded. We won’t cover how to do that here, but hopefully, it highlights why rendering on demand is trickier than using the loop. You have to consider all situations where you need a new frame (for example, don’t forget that you’ll also need to render a frame on resize).
-
-    viewer.start();
+  // complete async tasks
+  // Switch to a render load for improvement of running on lower power devices 
+  // produce a single frame (render on demand)
+  // viewer.render();
+  // rendering on demand, we are now only generating new frames when the user interacts with the scene and moves the camera. As soon as you move the controls, sure enough, a new frame will be created and the texture will show up.
+  // As a result, you also need to generate a new frame after the texture has loaded. We won’t cover how to do that here, but hopefully, it highlights why rendering on demand is trickier than using the loop. You have to consider all situations where you need a new frame (for example, don’t forget that you’ll also need to render a frame on resize).
 }
 
 // call main to start the app
 main().catch((err) => {
-    console.error(err);
+  console.error(err);
 });
